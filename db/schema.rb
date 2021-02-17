db51a1ef47a8d0fc6d0a4faba59e770362421e66
Nick McDaniel <nickamcd@gmail.com>
2021-02-17 13:57:11 -0800

    Created migration for post.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_210653) do
+ActiveRecord::Schema.define(version: 2021_02_17_215134) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

276f653e1a58fcbb3dcf6b612d45c25cc7d4f0b2
Nick McDaniel <nickamcd@gmail.com>
2021-02-17 14:26:38 -0800

    Created assocation between User and Post.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, uniqueness: true, length: { in: 4..12 }, presence: true
   validates :password, length: { in: 6..16 }, presence: true
 end
